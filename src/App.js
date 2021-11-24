d23a39250bb221465a30cc071d5311233d8d6de8
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-24 12:56:45 +0530

    created required components and Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Components/Navbar/NavbarElements';
 
 function App() {
   return (
     <div className="App">
-    
+      <Navbar/>
     </div>
   );
 }
---
+++ src/Components/Navbar/NavbarElements.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+            <h2>hello</h2>
+        </div>
+    )
+}
+
+export default Navbar
