bcfc973227022e0da39a1a981d3e1a412770fa93
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-26 12:48:15 +0530

    created main content of HomePage

--- src/Components/HeroSection/index.js
+++ src/Components/HeroSection/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import video from '../../Videos/video.mp4'
+import React,{useState} from 'react';
+import video from '../../Videos/video.mp4';
+import { Button } from '../ButtonElements';
 import {HeroContainer,
     HeroBg,
-    VideoBg
+    VideoBg,
+    HeroContent,
+    HeroH1,
+    HeroP,
+    HeroBtnWrapper,
+    ArrowForward,
+    ArrowRight
 } from './HeroElements'
 
 function HeroSection() {
+
+    const [hover, setHover] = useState(false);
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
     return (
         <>
         <HeroContainer>
             <HeroBg>
                 <VideoBg autoPlay loop muted src={video} type='video/mp4'/>
             </HeroBg>
+            <HeroContent>
+                <HeroH1> Virtual Banking Made Easy</HeroH1>
+                <HeroP>
+                    Sign Up for a new account today and recive $250 in 
+                    credit towards your next payment.
+                </HeroP>
+                <HeroBtnWrapper>
+                    <Button to="signup" 
+                    onMouseEnter={onHover}
+                    onMouseLeave={onHover}
+                    >
+                        Get started {hover ? <ArrowForward/> : <ArrowRight/>}
+                    </Button>
+                </HeroBtnWrapper>
+            </HeroContent>
         </HeroContainer>
         </>
     )
