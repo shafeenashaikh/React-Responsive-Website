d5f2d4a3a3d63cfd3c046dcdd858aa716e63a3fe
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-29 09:29:31 +0530

    created data for the about page

--- src/Components/InfoSection/Data.js
+++ src/Components/InfoSection/Data.js
@@ -1,4 +1,4 @@
-export const homeOnjOne = {
+export const homeObjOne = {
     id: 'about',
     lightBg: false,
     lightText: true,
@@ -8,9 +8,27 @@ export const homeOnjOne = {
     description: 'Get access to our exclusive app that allow you to send unlimited transactions without charged any fees.',
     buttonLable: 'Get started',
     imgStart: false,
-    img: require('../../Images/svg-1.svg'),
-    alt: 'Car',
+    img: require('../../Images/svg1.svg').default,
+    alt:'Car',
     dark: true,
     primary: true,
     darkText: false
 }
+
+// export const homeObjTwo = {
+//     id: 'discover',
+//     lightBg: true,
+//     lightText: false,
+//     lightTextDesc: false,
+//     topLine: 'Unlimited Access',
+//     headline: 'Unlimited Transactions with zero fees',
+//     description: 'Get access to our exclusive app that allow you to send unlimited transactions without charged any fees.',
+//     buttonLable: 'Learn More',
+//     imgStart: true,
+//     img: require('../../Images/svg2.svg').default,
+//     alt:'Car',
+//     dark: false,
+//     primary: false,
+//     darkText: true,
+// }
+
