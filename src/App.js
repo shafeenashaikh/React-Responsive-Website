a8ea2b9f5719d57e7a399ff15611043253de67d7
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-30 18:20:31 +0530

    created signin page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import  {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages';
+import SignInPage from './Pages/signin';
+import Navbar from './Components/Navbar/Index';
 
 
 function App() {
   return (
-  <Router>
-    <Home/>
-  </Router>
+    <Router>
+      <Navbar/>
+      <Routes>
+      <Route path="/" element={<Home/>}  />
+      <Route path="/signin" element={<SignInPage/>} />
+     </Routes>
+    </Router>
   );
 }
 
