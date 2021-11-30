152a5ad63e4bdaf217242b0a585d9f3a144c547c
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-11-30 20:18:46 +0530

    created Footer of page

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -1,4 +1,29 @@
-import React from 'react'
+import React from 'react';
+import {
+    FaFacebook,
+    FaInstagram,
+    FaTwitter,
+    FaLinkedin
+
+} from 'react-icons/fa'
+
+import {
+    FooterContainer,
+    FooterWrap,
+    FooterLinksContainer,
+    FooterLinkWrapper,
+    FooterLinkItems,
+    FooterLinkTitle,
+    FooterLink,
+    SocialMedia,
+    SocialMediaWrap,
+    SocialLogo ,
+    WebsiteRights,
+    SocialIcons,
+    SocialIconLink 
+} from './FooterElements';
+
+
 
 function Footer() {
     return (
@@ -15,33 +40,59 @@ function Footer() {
                                 <FooterLink to="/signin"> Terms of service</FooterLink>
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About Us</FooterLinkTitle>
-                                <FooterLink to="/signin"> How it work</FooterLink>
-                                <FooterLink to="/signin"> Testimonials</FooterLink>
-                                <FooterLink to="/signin"> Careers</FooterLink>
-                                <FooterLink to="/signin"> Investors</FooterLink>
-                                <FooterLink to="/signin"> Terms of service</FooterLink>
+                            <FooterLinkTitle>Contact Us</FooterLinkTitle>
+                                <FooterLink to="/">Contact</FooterLink>
+                                <FooterLink to="/">Support</FooterLink>
+                                <FooterLink to="/">Destination</FooterLink>
+                                <FooterLink to="/">Sponsorship</FooterLink>
                         </FooterLinkItems>
                     </FooterLinkWrapper>
                     <FooterLinkWrapper>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About Us</FooterLinkTitle>
-                                <FooterLink to="/signin"> How it work</FooterLink>
-                                <FooterLink to="/signin"> Testimonials</FooterLink>
-                                <FooterLink to="/signin"> Careers</FooterLink>
-                                <FooterLink to="/signin"> Investors</FooterLink>
-                                <FooterLink to="/signin"> Terms of service</FooterLink>
+                            <FooterLinkTitle> Videos</FooterLinkTitle>
+                                <FooterLink to="/">Submit Videos</FooterLink>
+                                <FooterLink to="/"> Ambassadors</FooterLink>
+                                <FooterLink to="/">Agency</FooterLink>
+                                <FooterLink to="/">Influencer</FooterLink>
+                                
                         </FooterLinkItems>
                         <FooterLinkItems>
-                            <FooterLinkTitle> About Us</FooterLinkTitle>
-                                <FooterLink to="/signin"> How it work</FooterLink>
-                                <FooterLink to="/signin"> Testimonials</FooterLink>
-                                <FooterLink to="/signin"> Careers</FooterLink>
-                                <FooterLink to="/signin"> Investors</FooterLink>
-                                <FooterLink to="/signin"> Terms of service</FooterLink>
+                            <FooterLinkTitle> Social Media</FooterLinkTitle>
+                                <FooterLink to="/">Instagram</FooterLink>
+                                <FooterLink to="/">Facebook</FooterLink>
+                                <FooterLink to="/">Youtube</FooterLink>
+                                <FooterLink to="/">Twitter</FooterLink>
                         </FooterLinkItems>
                     </FooterLinkWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to="/">
+                            dolla
+                        </SocialLogo>
+                        <WebsiteRights>dolla @2020
+                            All rights reserved.
+                        </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/" target="_blank"
+                                aria-label="Facebook">
+                                <FaFacebook/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                                aria-label="Instagram">
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                                aria-label="Twitter">
+                                <FaTwitter/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/" target="_blank"
+                                aria-label="Linkedin">
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
