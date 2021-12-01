498c37d0057e06d85d67e2d78aef80fb3272e994
shafeenashaikh <shafeenashaikh99297@gmail.com>
2021-12-01 10:48:37 +0530

    back to homescreen on click of home using react-scroll

--- src/Components/Navbar/Index.js
+++ src/Components/Navbar/Index.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import {FaBars} from 'react-icons/fa';
 import {IconContext} from 'react-icons/lib';
+import { animateScroll as scroll } from 'react-scroll';
 import { Nav,
     NavbarContainer,
     NavLogo,
@@ -26,14 +27,18 @@ function Navbar({toggle}) {
 
     useEffect(()=>{
         window.addEventListener('scroll', changeNav)
-    }, [])
+    }, []);
+
+    const toggleHome = () => {
+        scroll.scrollToTop()
+    }
 
     return (
        <>
        <IconContext.Provider value={{color: '#fff'}}>
         <Nav scrollNav={scrollNav}>
            <NavbarContainer>
-               <NavLogo to='/'>Home</NavLogo>
+               <NavLogo to='/' onClick={toggleHome}>Home</NavLogo>
                <MobileIcon onClick={toggle}>
                    <FaBars/>
                </MobileIcon>
